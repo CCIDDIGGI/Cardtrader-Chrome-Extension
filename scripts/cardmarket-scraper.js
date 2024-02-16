35322c154f6a4869040147ad60ab3e6e40029ce4
CCIDDIGGI <marco.boccolacci@gmail.com>
2024-02-16 15:46:06 +0100

    "undistributed" version, previous to the webpack package - savepoint

--- scripts/cardmarket-scraper.js
+++ scripts/cardmarket-scraper.js
@@ -1,8 +1,18 @@
-import { RequestQueue } from '../node_modules/@crawlee';
+import { RequestQueue, CheerioCrawler } from '../node_modules/@crawlee';
 
 const requestQueue = await RequestQueue.open();
 
 await requestQueue.addRequest({ url: 'https://crawlee.dev' })
+
+const crawler = new CheerioCrawler({
+    requestQueue,
+    async requestHandler({ $, request }) {
+        const title = $('title').text();
+        console.log(`the title of "${request.url}" is: ${title}`);
+    }
+})
+
+await crawler.run();
 
 document.getElementById("btn").addEventListener("click", print());
 
